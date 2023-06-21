1d014178811d114cc959934c188382a28240e29e
Abhinav Pratap <Abhi@192.168.1.19>
2023-06-21 17:08:56 +0530

    added 2 new pages

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.DB_URI).then((data) => { console.log('Database connected'); });
 
 const userSchema = new mongoose.Schema({
+    name: String,
     email: String,
     password: String
 });
@@ -40,14 +41,17 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async(req,res)=>{
     const newUser = new User({
+        name: req.body.name,
         email: req.body.username,
         password: md5(req.body.password)
     });
+    const name = req.body.name;
     try{
         newUser.save();
-        res.render("secrets");
+        res.render("secrets", {name: name});
     }
     catch(err){
+        res.render("login_failed");
         console.log(err);
     }
 });
@@ -55,15 +59,16 @@ app.post("/register", async(req,res)=>{
 app.post("/login", async(req,res)=>{
     const username = req.body.username;
     const password = md5(req.body.password);
-
+    const name = req.body.name;
     try{
         const foundUser = await User.findOne({email: username});
         if(foundUser.password === password){
-            res.render("secrets");
+            res.render("secrets", {name: foundUser.name});
         }
     }
     catch(err){
-        console.log(err);
+        console.log(`error: ${err}`);
+        res.render("not_found");
     }
 });
 
